d5b128086e9346b61a1311eec18dd0904306e943
OZhang <zhangrunze@163.com>
2017-09-22 19:53:23 +0800

    Add PV800 image

--- components/images.js
+++ components/images.js
@@ -2,5 +2,6 @@ module.exports = {
      '450L' : require('../img/device/450L.png'),
      '2080-LC20-20QBB_Micro820' : require('../img/device/2080-LC20-20QBB_Micro820.png'),
      '440C' : require('../img/device/440C.png'),
+     '2711R-T7T' : require('../img/device/2711R-T7T.png'),
     /* etc... */
   };
