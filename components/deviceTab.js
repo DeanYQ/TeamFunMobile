7e0648ac62eb470096e3cbe76609000af943722c
oliver <ozhang@ra.rockwell.com>
2017-09-29 15:26:48 +0800

    Fix missing code

--- components/deviceTab.js
+++ components/deviceTab.js
@@ -17,24 +17,8 @@ import { StackNavigator } from 'react-navigation';
 import AuthService from '../AuthService';
 import DeviceDetailPage from './deviceDetailPage';
 const onButtonPress = () => {
-    //Alert.alert('Button has been pressed!')
-    //React.findDOMNode(this.refs.Te).focus();
-    //Alert.alert(document.activeElement.tagName);
 };
 var token = null;
-
-// class DeviceDetail extends React.Component {
-//     static navigationOptions = {
-//       title: 'Device Detail',
-//     };
-//     render() {      
-//       return (
-//         <View>
-//             <Text>dfdf</Text>
-//         </View>
-//       );
-//     }
-//   }
 
 class DeviceHomeScreen extends React.Component {
     constructor(props) {
@@ -54,7 +38,7 @@ class DeviceHomeScreen extends React.Component {
                 style={{ backgroundColor: '#960000', marginRight: 10 }}
                 imageStyle={{ width: 25, height: 25 }}
                 imageSource={require('../img/searchAdd.png')}
-                onPress={ () => {
+                onPress={() => {
                     Alert.alert(this.state.message);
                 }}>
             </ImageButton>
@@ -66,15 +50,17 @@ class DeviceHomeScreen extends React.Component {
 
 
     requestDevices(callback) {
-        this.getDevices((response) => {
+        var url = "http://******yourapi***************/api/Devicelist";
+        this.getDevices(url, (response) => {
             return callback(response);
         });
     }
 
-    getDevices(callback) {
+    getDevices(url, callback) {
+        console.log(url);
         AuthService.getAuthInfo((err, info) => {
             var token = info;
-            fetch("http://mobileservices20170819084039.azurewebsites.net/api/Devicelist", {
+            fetch(url, {
                 method: "GET",
                 headers: {
                     'Authorization': 'Bearer ' + token
@@ -90,22 +76,10 @@ class DeviceHomeScreen extends React.Component {
         });
     }
 
+
     render() {
         return (
             <View style={{ flex: 1 }}>
-                {/* <View style={{ flex: 1, height: 60, backgroundColor: '#333333', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Text style={{ color: 'white', fontSize: 18, width: 200, backgroundColor: 'transparent', marginLeft: 5 }}>Mobile Detector</Text>
-                    <ImageButton
-                        style={{ backgroundColor: 'transparent', marginRight: -80 }}
-                        imageStyle={{ width: 25, height: 25 }}
-                        imageSource={require('../img/refresh.png')}>
-                    </ImageButton>
-                    <ImageButton
-                        style={{ backgroundColor: 'transparent', marginRight: 10 }}
-                        imageStyle={{ width: 25, height: 25 }}
-                        imageSource={require('../img/searchAdd.png')}>
-                    </ImageButton>
-                </View> */}
                 <View style={{ flex: 10 }}>
                     <DeviceList itemsource={this.props.navigation.state.ds} onRefresh={this.requestDevices.bind(this)}
                         navigation={this.props.navigation} />
@@ -120,75 +94,6 @@ const DeviceTab = StackNavigator({
     Detail: { screen: DeviceDetailPage },
 
 })
-
-// class DeviceTab extends Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             message: 't',
-//             ds:[]
-//         }
-//     }
-
-//     onAddBtnPress() {
-//         Alert.alert(this.state.message);
-//     }
-
-
-//     getDevices(callback) {
-//         AuthService.getAuthInfo((err, info) => {
-//             var token = info;
-//             fetch("http://mobileservices20170819084039.azurewebsites.net/api/Devicelist", {
-//                     method: "GET",
-//                     headers: {
-//                         'Authorization': 'Bearer ' + token
-//                     }
-//                 })
-//                 .then((response) => {
-
-//                     var newData = JSON.parse(response._bodyInit);
-//                     return callback(newData);
-
-//                 })
-//                 .done();
-//         });
-//     }
-
-//     requestDevices(callback) {
-//         this.getDevices((response) => {
-//             return callback(response);
-//         });
-
-//     }
-
-
-
-//     render() {
-//         return (
-//             <View style={{ flex:1}}>
-//                 <View style={{ flex:1,height: 60, backgroundColor: '#333333', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-//                     <Text style={{ color: 'white', fontSize: 18, width: 200, backgroundColor: 'transparent', marginLeft: 5 }}>Mobile Detector</Text>
-//                     <ImageButton
-//                         style={{ backgroundColor: 'transparent', marginRight: -80 }}
-//                         onPress={this.onAddBtnPress.bind(this)}
-//                         imageStyle={{ width: 25, height: 25 }}
-//                         imageSource={require('../img/refresh.png')}>
-//                     </ImageButton>
-//                     <ImageButton
-//                         style={{ backgroundColor: 'transparent', marginRight: 10 }}
-//                         onPress={this.onAddBtnPress.bind(this)}
-//                         imageStyle={{ width: 25, height: 25 }}
-//                         imageSource={require('../img/searchAdd.png')}>
-//                     </ImageButton>
-//                 </View>
-//                 <View style={{ flex:10}}>
-//                     <DeviceList itemsource={this.state.ds} onRefresh={this.requestDevices.bind(this)} />
-//                 </View>
-//             </View>
-//         );
-//     }
-// }
 
 var styles = StyleSheet.create({
     header: {
