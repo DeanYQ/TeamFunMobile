b2636f2c3765d302dcb9ff38ea58665f9491f57e
OZhang <zhangrunze@163.com>
2017-11-25 20:44:01 +0800

    android push notification

--- android/app/src/main/java/com/mobileapplication/MainApplication.java
+++ android/app/src/main/java/com/mobileapplication/MainApplication.java
@@ -14,6 +14,11 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  // 设置为 true 将不弹出 toast
+  private boolean SHUTDOWN_TOAST = false;
+  // 设置为 true 将不打印 log
+  private boolean SHUTDOWN_LOG = false;
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -24,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new JPushPackage()
+            new JPushPackage(SHUTDOWN_TOAST, SHUTDOWN_LOG)
       );
     }
   };
