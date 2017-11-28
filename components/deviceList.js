c9ec4b9944a36a1f47cf63b6ef9268db631a9601
oliver <ozhang@ra.rockwell.com>
2017-11-28 16:51:37 +0800

    remove device

--- components/deviceList.js
+++ components/deviceList.js
@@ -16,7 +16,7 @@ import {
 import ImageButton from './imageButton';
 import images from './images';
 import { StackNavigator, } from 'react-navigation';
-const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+// const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 import Signalr from './Signalr/deviceSignalr';
 import Swipeable from 'react-native-swipeable';
 import AuthService from '../AuthService';
@@ -26,29 +26,33 @@ class DeviceList extends Component {
         super(props);
         this.state = {
             refreshing: false,
+            rerender: false,
+            currentlyOpenSwipeable: null,
             signalrConnecting: false,
             itemsource: this.props.itemsource,
-            proxy: null
+            proxy: null,
+            clientId: null
         };
     }
 
     componentDidMount() {
         this._signalrConnection();
-        this._onRefresh();
+        this._onRefresh(()=>{});
     }
 
     _signalrConnection() {
         this.setState({ signalrConnecting: true });
 
-        Signalr.connect(((callback) => {
+        Signalr.connect(((callback, Id) => {
             this.setState({
                 signalrConnecting: false,
-                proxy: callback
+                proxy: callback,
+                clientId: Id
             }, () => console.log('get-data-from-server:' + this.state.proxy));
         }));
     }
 
-    _onRefresh() {
+    _onRefresh(cb) {
         this.setState({ refreshing: true });
 
         this.props.onRefresh((response) => {
@@ -57,11 +61,12 @@ class DeviceList extends Component {
                 refreshing: false
             });
             console.log(this.state.itemsource);
+            cb();
         });
     }
 
     fetchData() {
-        return this.props.onRefresh();
+        return this.props.onRefresh(()=>{});
     }
 
     pressItem(item) {
@@ -78,41 +83,17 @@ class DeviceList extends Component {
             return;
         if (this.state.refreshing)
             return;
-            console.log(item.Id);    
-         // TODO: send request to the server
-         const { navigate } = this.props.navigation;
-         var url = "http://mobileservices20170819084039.azurewebsites.net/api/Devicelist";
-         var params = {
-             Catalog: item.Catalog,
-             DeviceType: item.DeviceType,
-             ProductType: item.ProductType,
-             Rev: item.Rev,
-             IP: item.IP
-         };
-         var formBody = [];
-         for (var property in params) {
-             var encodedKey = encodeURIComponent(property);
-             var encodedValue = encodeURIComponent(params[property]);
-             formBody.push(encodedKey + "=" + encodedValue);
-         }
-         var data = formBody.join("&");
-         console.log(data);
-         AuthService.getAuthInfo((err, info) => {
-             var token = info;
-             fetch(url, {
-                 method: "Delete",
-                 headers: {
-                     'Content-Type': 'application/x-www-form-urlencoded',
-                     'Authorization': 'Bearer ' + token
-                 },
-                 body: data
-             })
-             .then((response) => {
-                console.log(response);
-                this._onRefresh();
-            })
-             .done();
-         });
+        Signalr.DeleteDevice(this.state.proxy,
+            this.state.clientId,
+            item.Catalog.trim(),
+            item.IP.trim(), () => {
+                console.log("device deleted");
+                this._onRefresh(() => {
+                this.setState({
+                    rerender: true
+                });
+            });
+            });
     }
 
     _renderItem = ({ item }) => {
@@ -135,22 +116,12 @@ class DeviceList extends Component {
         if (imageSrc != "") {
             return (
                 <Swipeable
-                    // leftContent={(
-                    //     <View style={[styles.leftSwipeItem, { backgroundColor: 'lightskyblue' }]}>
-                    //         <Text>Pull action</Text>
-                    //     </View>
-                    // )}
                     rightButtons={[
                         <TouchableOpacity style={[styles.rightSwipeItem, { backgroundColor: 'red' }]}
-                        onPress={_ => this.deleteItem(item)}>
-                            <Text style={{color: 'white',fontWeight: 'bold'}}>Delete</Text>
+                            onPress={_ => this.deleteItem(item)}>
+                            <Text style={{ color: 'white', fontWeight: 'bold' }}>Delete</Text>
                         </TouchableOpacity>,
-                        // <TouchableOpacity style={[styles.rightSwipeItem, { backgroundColor: 'orchid' }]}>
-                        //     <Text>2</Text>
-                        // </TouchableOpacity>
                     ]}
-                    // onRightButtonsOpenRelease={onOpen}
-                    // onRightButtonsCloseRelease={onClose}
                 >
                     <TouchableOpacity onPress={_ => this.pressItem(item)}>
                         <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center', backgroundColor: '#fff', borderColor: '#D7D7D7', borderBottomWidth: 1 }}>
@@ -169,29 +140,7 @@ class DeviceList extends Component {
                             </View>
                         </View>
                     </TouchableOpacity>
-                    {/* <View style={[styles.listItem, {backgroundColor: 'salmon'}]}>
-                  <Text>Example 1</Text>
-                </View> */}
                 </Swipeable>
-
-
-                // <TouchableOpacity onPress={_ => this.pressItem(item)}>
-                //     <View style={{ flexDirection: 'row', padding: 10, alignItems: 'center', backgroundColor: '#fff', borderColor: '#D7D7D7', borderBottomWidth: 1 }}>
-                //         <Image source={imageSrc} style={{ height: 50, width: 50, padding: 20, margin: 5 }}>
-                //         </Image>
-                //         <View style={{ paddingLeft: 20 }}>
-                //             <Text style={{ fontSize: 20 }}>
-                //                 {item.Catalog}
-                //             </Text>
-                //             <Text style={{ fontSize: 15 }}>
-                //                 IP Address: {item.IP}
-                //             </Text>
-                //             <Text style={{ fontSize: 15 }}>
-                //                 Rev: {item.Rev}
-                //             </Text>
-                //         </View>
-                //     </View>
-                // </TouchableOpacity>
             )
         }
     };
@@ -199,7 +148,7 @@ class DeviceList extends Component {
     render() {
         return (
             <View style={{ justifyContent: 'flex-start' }}>
-                <AnimatedFlatList
+                <FlatList 
                     refreshControl={
                         <RefreshControl
                             refreshing={this.state.refreshing || this.state.signalrConnecting}
@@ -209,6 +158,7 @@ class DeviceList extends Component {
                         />}
 
                     data={this.state.itemsource}
+                    extraData={this.state.rerender}
                     renderItem={this._renderItem}
                     refreshing={false}
                 />
